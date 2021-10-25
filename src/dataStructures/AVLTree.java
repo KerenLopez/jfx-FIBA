583f1a2f2430d69dae13d7a81e1f2c84ca045c5e
DianaOlanoU <1005967885@u.icesi.edu.co>
2021-10-24 20:18:50 -0500

    AVL tree classes modification

--- src/dataStructures/AVLTree.java
+++ src/dataStructures/AVLTree.java
@@ -18,22 +18,23 @@ public class AVLTree  <K extends Comparable<K>,V> implements IAVLTree<K,V>{
     public void setRoot(NodeAVL<K, V> root) {
         this.root = root;
     }
-    
-    public NodeAVL<K,V>  privateSearch(K key) {
-        return search(root,key);
-    }
 
     @Override
-    public NodeAVL<K,V> search(NodeAVL<K,V> node,K key) {
+    public NodeAVL<K,V> search(K key) {
+        NodeAVL<K,V> founded=privateSearch(root,key);
+        return founded;
+    }
+    
+    public NodeAVL<K,V> privateSearch(NodeAVL<K,V> node, K key) {
         if(node==null || node.compareTo(key)==0) {
             return node;
         }
         else {
             if(node.compareTo(key)<0) {
-                return search(node.getLeft(), key);
+                return privateSearch(node.getLeft(),key);
             }
             else{
-                return search(node.getRight(), key);
+                return privateSearch(node.getRight(),key);
             }
         }
     }
@@ -45,20 +46,20 @@ public class AVLTree  <K extends Comparable<K>,V> implements IAVLTree<K,V>{
         if(root==null) {
             root = newNode;
         }else {
-            insertNodeRecursive(root, newNode);
+            privateInsert(root, newNode);
             inserted = true;
         }
         return inserted;
     }
     
-    private void insertNodeRecursive( NodeAVL<K,V> node,NodeAVL<K,V> newNode) {
-        if(newNode.compareTo(node.getKey())<0){
+    private void privateInsert( NodeAVL<K,V> node,NodeAVL<K,V> newNode) {
+        if(newNode.compareTo(node.getKey())<=0){
             if(node.getLeft()==null){
                 node.setLeft(newNode);
                 newNode.setParent(node);
             }
             else{
-                insertNodeRecursive(node.getLeft(),newNode);
+                privateInsert(node.getLeft(),newNode);
             }
         }
         else{
@@ -67,31 +68,104 @@ public class AVLTree  <K extends Comparable<K>,V> implements IAVLTree<K,V>{
                 newNode.setParent(node);
             }
             else{
-                insertNodeRecursive(node.getRight(),newNode);
+                privateInsert(node.getRight(),newNode);
             }	
+        }
+        newNode.setHeight(Math.max(height(newNode.getLeft()), height(newNode.getRight()))+1);
+        balance(newNode);
+    }
+
+    @Override
+    public boolean deleted(K key) {
+        Boolean deleted = false;
+        NodeAVL<K,V> founded = search(key);
+        if(founded!=null) {
+                deletedPrivate(founded);
+                deleted = true;
+        }
+        return deleted;
+    }
+    
+    private void deletedPrivate(NodeAVL<K,V> node){
+        if(node.getLeft()==null && node.getRight()==null){
+            if(node==root){
+                root=null;
+            }
+            else if(node==node.getParent().getLeft()){
+                node.getParent().setLeft(null);		
+            }
+            else{
+                node.getParent().setRight(null);;
+            }
+            node.setParent(null);
+        }
+        
+        else if(node.getLeft()==null || node.getRight()==null){
+            NodeAVL<K,V> onlyChild;
+            if(node.getLeft()!=null){
+                onlyChild=node.getLeft();
+                node.setLeft(null);
+            }
+            else{
+                onlyChild=node.getRight();
+                node.setRight(null);
+            }
+            
+            onlyChild.setParent(node.getParent());
+            
+            if(node==root){
+                root=onlyChild;
+            }
+            else if(node==node.getParent().getLeft()){
+                node.getParent().setLeft(onlyChild);	
+            }
+            else{
+                node.getParent().setRight(onlyChild);
+            }
+            node.setParent(null);
+        }
+        
+        else{
+            NodeAVL<K,V> sucesor=minValueNode(node.getRight());
+            node.setValue(sucesor.getValue());
+            deletedPrivate(sucesor);
+        }
+        
+        root.setHeight(Math.max(height(root.getLeft()),height(root.getRight()))+1);
+        int balance=GetBalancingFactor(root);
+        
+        if (balance>1 && GetBalancingFactor(root.getLeft())>=0){
+            rotateRight(root);
+        }
+        if (balance>1 && GetBalancingFactor(root.getLeft())<0) {
+            root.setLeft(rotateLeft(root.getLeft()));
+            rotateRight(root);
+        }
+        if (balance < -1 && GetBalancingFactor(root.getRight()) <= 0){
+            rotateLeft(root);
+        }
+        if (balance<-1 && GetBalancingFactor(root.getRight()) > 0) {
+            root.setRight(rotateRight((NodeAVL<K,V>) root.getRight()));
+            rotateLeft(root);
         }
     }
 
     @Override
-    public void deleted(K key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void rotateLeft(NodeAVL<K,V> node) {
+    public NodeAVL<K,V> rotateLeft(NodeAVL<K,V> node) {
         node.getRight().setLeft(node);
         node.setRight(node.getRight().getLeft());
         node.setHeight(Math.max(height(node.getLeft()), height(node.getRight()))+1);
         node.getRight().setHeight(Math.max(height(node.getRight().getLeft()), height(node.getRight().getRight())) + 1);
-        
+        return node.getLeft();
     }
 
     @Override
-    public void rotateRight(NodeAVL<K, V> node) {
+    public NodeAVL<K,V> rotateRight(NodeAVL<K, V> node) {
         node.getLeft().setRight(node);
         node.setLeft(node.getLeft().getRight());
         node.setHeight(Math.max(height(node.getLeft()), height(node.getRight())) + 1);
         node.getLeft().setHeight(Math.max(height(node.getLeft().getLeft()), height(node.getLeft().getRight())) + 1);
+        return node.getRight();
     }
 
     @Override
@@ -100,14 +174,7 @@ public class AVLTree  <K extends Comparable<K>,V> implements IAVLTree<K,V>{
             return 0;
         }
         else {
-            int l = height(node.getLeft());
-            int r = height(node.getRight());
-            if (l>r) {
-                return (l+1);
-            } 
-            else {
-                return (r+1);
-            }
+            return node.getHeight();
         }
     }
     
@@ -140,6 +207,26 @@ public class AVLTree  <K extends Comparable<K>,V> implements IAVLTree<K,V>{
     @Override
     public ArrayList<V> showTree() {
        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public NodeAVL<K, V> minValueNode(NodeAVL<K, V> node){
+        while(node.getLeft()!=null){
+        node=(NodeAVL<K, V>)node.getLeft();
+        }
+        return node;
+    }
+    
+    public String preOrder(NodeAVL<K,V> node) {
+        String k="";
+        if (node==null) {
+            return k;
+        }
+        else{
+            k+=node.getKey()+" ";
+            k+=preOrder(node.getLeft());
+            k+=preOrder(node.getRight());
+        }
+        return k;
     }
     
     
--- src/dataStructures/IAVLTree.java
+++ src/dataStructures/IAVLTree.java
@@ -4,11 +4,11 @@ package dataStructures;
 import java.util.ArrayList;
 
 public interface IAVLTree<K extends Comparable<K>, V> {
-    public NodeAVL<K,V> search(NodeAVL<K,V> node, K key);
+    public NodeAVL<K,V> search(K key);
     public boolean insert(K key,V value);
-    public void deleted(K key);
-    public void rotateLeft(NodeAVL<K,V> node);
-    public void rotateRight(NodeAVL<K,V> node);
+    public boolean deleted(K key);
+    public NodeAVL<K,V> rotateLeft(NodeAVL<K,V> node);
+    public NodeAVL<K,V> rotateRight(NodeAVL<K,V> node);
     public int height(NodeAVL<K,V> node);
     public int GetBalancingFactor(NodeAVL<K,V> node);
     public ArrayList<V> showTree();
