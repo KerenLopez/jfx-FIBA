17347a7e25593c1d521263865e961b45537cf09c
DianaOlanoU <1005967885@u.icesi.edu.co>
2021-11-01 12:30:40 -0500

    AVL tree modifications and added search and delete methods

--- src/dataStructures/IAVLTree.java
+++ src/dataStructures/IAVLTree.java
@@ -9,9 +9,5 @@ public interface IAVLTree<K extends Comparable<K>, V> {
     public boolean delete(K key);
     public NodeAVL<K,V> leftRotate(NodeAVL<K,V> node);
     public NodeAVL<K,V> rightRotate(NodeAVL<K,V> node);
-    //public int getHeight(NodeAVL<K,V> node);
-    //public int getBalancingFactor(NodeAVL<K,V> node);
-    //public void reBalance(NodeAVL<K,V> node);
-    //public NodeAVL<K, V> getnodeMaximumValue(NodeAVL<K, V> node);
-    public ArrayList<NodeAVL<K,V>> preOrder();
+    public ArrayList<NodeAVL<K,V>> inorderTraversal();
 }
